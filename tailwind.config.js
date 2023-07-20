a5c20cf56f6673ada0c9d9eda868e5346e0519f1
Jonathan Lowell <elderlowell@gmail.com>
2023-07-19 23:11:54 -0600

    Third commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'dark-gray': '#525252',
+        'medium-gray': '#818488',
+        'light-gray': '#EFF2F6',
+        'tan': '#BFB79A'
+      }
+    },
+  },
+  plugins: [],
+}
